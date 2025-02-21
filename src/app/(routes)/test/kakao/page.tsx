5e01872564714bc7e5bb8bce5cc240b81eead0f2
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2025-02-21 17:01:26 +0900

    feat: test kakao map

---
+++ src/app/(routes)/test/kakao/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { useEffect, useRef } from "react"
+
+export default function Home() {
+  const kakaoMapContainer = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const kakao = window?.kakao
+    console.log("?", kakaoMapContainer)
+    console.log("?", kakao)
+    if (kakaoMapContainer?.current && typeof kakao !== "undefined") {
+      const container = kakaoMapContainer.current
+      console.log("fuck")
+      kakao.maps.load(() => {
+        const options = {
+          center: new kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3
+        }
+
+        const map = new kakao.maps.Map(container, options)
+      })
+    }
+  }, [kakaoMapContainer])
+
+  return (
+    <div>
+      <div id="map" ref={kakaoMapContainer} className="h-[500px] w-[500px]"></div>
+    </div>
+  )
+}
